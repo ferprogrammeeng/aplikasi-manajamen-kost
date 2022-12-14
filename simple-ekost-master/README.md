276a53855fbf36803b152ab894da98aa5f1c4878
Ferdi Ardiansyah <120326313+ferprogrammeeng@users.noreply.github.com>
2022-12-15 04:03:08 +0700

    Add files via upload

---
+++ simple-ekost-master/README.md
@@ -0,0 +1,25 @@
+## Aplikasi pencarian kost sederhana dengan google maps
+
+### Note
+Aplikasi ini membutuhkan koneksi internet untuk menampilkan maps dari google.
+
+### Cara menjalankan
+1. Clone repository dengan perintah
+```
+git clone https://github.com/imamdigmi/simple-ekost.git
+```
+2. Letakkan directory `simple-ekost` di dalam root directory web server
+3. Import database.sql
+4. Ubah Username dan Password pada file config.php
+5. Lalu browse ke ``localhost/simple-ekost`` di browser
+
+### Dummy test
+#### Administrator
+1. Browse ke **localhost/ekost/admin**
+2. Isi username dengan `admin`
+2. Isi password dengan `admin`
+
+#### Pemilik Kost
+1. Pada form login di menu sebelah kanan atas
+2. Isi username dengan `pemilik`
+2. Isi password dengan `pemilik`
---
+++ simple-ekost-master/database.sql
@@ -0,0 +1,172 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Feb 05, 2017 at 01:41 PM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 7.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ekost`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id_admin` int(5) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id_admin`, `username`, `password`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `galeri`
+--
+
+CREATE TABLE `galeri` (
+  `id_galeri` int(11) NOT NULL,
+  `id_kost` int(11) NOT NULL,
+  `judul` varchar(40) NOT NULL,
+  `file` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kost`
+--
+
+CREATE TABLE `kost` (
+  `id_kost` int(11) NOT NULL,
+  `id_pemilik` int(11) NOT NULL,
+  `nama` varchar(20) NOT NULL,
+  `alamat` varchar(50) NOT NULL,
+  `latitude` float NOT NULL,
+  `longitude` float NOT NULL,
+  `tersedia` int(2) NOT NULL,
+  `status` enum('Laki-laki','Perempuan') DEFAULT NULL,
+  `fasilitas` text NOT NULL,
+  `harga_3bulan` int(9) NOT NULL,
+  `harga_6bulan` int(9) NOT NULL,
+  `harga_pertahun` int(9) NOT NULL,
+  `pengunjung` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pemilik`
+--
+
+CREATE TABLE `pemilik` (
+  `id_pemilik` int(11) NOT NULL,
+  `nama` varchar(20) NOT NULL,
+  `alamat` varchar(30) NOT NULL,
+  `telepon` varchar(12) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pemilik`
+--
+
+INSERT INTO `pemilik` (`id_pemilik`, `nama`, `alamat`, `telepon`, `email`, `username`, `password`) VALUES
+(1, 'Pemilik Kost', 'Karangbendo Wetan RT. 02 RW.08', '081234567890', 'pemilik@gmail.com', 'pemilik', '58399557dae3c60e23c78606771dfa3d');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indexes for table `galeri`
+--
+ALTER TABLE `galeri`
+  ADD PRIMARY KEY (`id_galeri`),
+  ADD KEY `fk_kost` (`id_kost`);
+
+--
+-- Indexes for table `kost`
+--
+ALTER TABLE `kost`
+  ADD PRIMARY KEY (`id_kost`),
+  ADD KEY `fk_pemilik` (`id_pemilik`);
+
+--
+-- Indexes for table `pemilik`
+--
+ALTER TABLE `pemilik`
+  ADD PRIMARY KEY (`id_pemilik`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id_admin` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `galeri`
+--
+ALTER TABLE `galeri`
+  MODIFY `id_galeri` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `kost`
+--
+ALTER TABLE `kost`
+  MODIFY `id_kost` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `pemilik`
+--
+ALTER TABLE `pemilik`
+  MODIFY `id_pemilik` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `galeri`
+--
+ALTER TABLE `galeri`
+  ADD CONSTRAINT `galeri_ibfk_1` FOREIGN KEY (`id_kost`) REFERENCES `kost` (`id_kost`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `kost`
+--
+ALTER TABLE `kost`
+  ADD CONSTRAINT `kost_ibfk_1` FOREIGN KEY (`id_pemilik`) REFERENCES `pemilik` (`id_pemilik`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
